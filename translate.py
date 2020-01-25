163e3bd58af5bbde35d4dcff596919a4e70e0b04
Ramana Murugesan <33397706+ramanamuru@users.noreply.github.com>
2020-01-25 17:54:05 -0500

    added auth key and basic translation

--- translate.py
+++ translate.py
@@ -5,13 +5,13 @@ import argparse
 import six
 
 """ Translates provided orig_text to target language string """
-def translate(orig_text):
+def translate(text):
     from google.cloud import translate_v2 as translate
     translate_client = translate.Client()
     target = "fr" #change this later
     if isinstance(text, six.binary_type):
         text = text.decode('utf-8')
-    result = translate_client.translate(text, target_language = target)
+    result = translate_client.translate(text, target_language = "en")
 
     print(u'Text: {}'.format(result['input']))
     print(u'Translation: {}'.format(result['translatedText']))
