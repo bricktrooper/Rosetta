7d821dada413b124f5ec2805e7d9d4c8c61b26da
Kyle Pinto <kylepinto@yahoo.ca>
2020-01-25 21:23:20 -0500

    Working sample code
    - Changed format to 'flac'
    - changed bitrate to 44100

--- speech2text.py
+++ speech2text.py
@@ -10,20 +10,17 @@ from google.cloud.speech import types
 client = speech.SpeechClient()
 
 # The name of the audio file to transcribe
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    'resources',
-    'audio.raw')
+file_name = os.path.join(os.path.dirname(__file__), '.', 'sample.flac')
 
 # Loads the audio into memory
 with io.open(file_name, 'rb') as audio_file:
     content = audio_file.read()
-    audio = types.RecognitionAudio(content=content)
+    audio = types.RecognitionAudio(content = content)
 
 config = types.RecognitionConfig(
-    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-    sample_rate_hertz=16000,
-    language_code='en-US')
+    encoding = enums.RecognitionConfig.AudioEncoding.FLAC,
+    sample_rate_hertz = 44100,
+    language_code = 'en-US')
 
 # Detects speech in the audio file
 response = client.recognize(config, audio)
