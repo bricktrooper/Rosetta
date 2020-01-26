8ddccac9fdddb58339e76857c0d75ae6af5d0d9b
Kyle Pinto <kylepinto@yahoo.ca>
2020-01-26 03:12:43 -0500

    Made s2t and t2s more modular

--- speech2text.py
+++ speech2text.py
@@ -10,15 +10,11 @@ from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
 
-AUDIO_ENCODING = enums.RecognitionConfig.AudioEncoding.LINEAR16
-SAMPLE_RATE = 16000   # (in Hertz)
+INPUT_AUDIO_ENCODING = enums.RecognitionConfig.AudioEncoding.LINEAR16
+INPUT_SAMPLE_RATE = 16000   # (in Hertz)
 INPUT_LANGUAGE = 'en-US'
-VIDEO_FILE = 'input2.mp4'
+INPUT_VIDEO_FILE = 'input1.mp4'
 
-audio_info = types.RecognitionConfig(
-		encoding = AUDIO_ENCODING,
-		sample_rate_hertz = SAMPLE_RATE,
-		language_code = INPUT_LANGUAGE)
 speech2text_client = speech.SpeechClient()
 
 # decodes an mp4 video into raw audio
@@ -37,17 +33,24 @@ def extract_audio(in_filename, **input_kwargs):
 
 # transcribes the audio to text and saves transcript to a file
 def transcribe(file_name):
+
+	# update audio config info
+	audio_config = types.RecognitionConfig(
+		encoding = INPUT_AUDIO_ENCODING,
+		sample_rate_hertz = INPUT_SAMPLE_RATE,
+		language_code = INPUT_LANGUAGE)
+
 	print("Audio input settings:")
-	print("Audio Encoding:    %s" % AUDIO_ENCODING)
-	print("Sample Rate (Hz):  %s" % SAMPLE_RATE)
+	print("Audio Encoding:    %s" % INPUT_AUDIO_ENCODING)
+	print("Sample Rate (Hz):  %s" % INPUT_SAMPLE_RATE)
 	print("Input Language:    %s" % INPUT_LANGUAGE)
 
 	print("Converting mp4 to raw audio .......")
-	audio = extract_audio(VIDEO_FILE)
+	audio = extract_audio(INPUT_VIDEO_FILE)
 
 	print("Transcribing .......")
 	file = open(file_name, "a")
-	response = speech2text_client.recognize(audio_info, audio)
+	response = speech2text_client.recognize(audio_config, audio)
 	for result in response.results:
 		file.write(result.alternatives[0].transcript)
 
