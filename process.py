f450e9d6bb21346e265db1d89a5c5c66eb992a66
Kyle Pinto <kylepinto@yahoo.ca>
2020-01-26 10:07:28 -0500

    Fix syntax errors

--- process.py
+++ process.py
@@ -1,41 +1,36 @@
 # Have you ever heard the tragedy of Darth Plagueis the Wise?
 
+from speech2text import *
+from translate import *
+from text2speech import *
 
 #return 3-tuple, 1st elem = name of translated mp3, 2nd elem = name of translated text, 3rd elem = original text
 # call with NO .p4 extension
 def process(mp4_file= "tests/trudeau", input_lang= "fr",target_lang="en"):
 
-
-    from speech2text import *
-    from translate import *
-    from text2speech import *
-
     transcript = transcribe((mp4_file+".mp4"), "tests/translated", addDialect(input_lang))
-
 
     translated_script = translate(transcript, target_lang)
     print(translated_script)
 
     mp3_translated_name =  mp4_file+"_translated.mp3"
-
-
     speak(translated_script, addDialect(target_lang), mp3_translated_name)
 
-    return (mp3_translated_name, translated_scrjpt, transcript)
+    return (mp3_translated_name, translated_script, transcript)
 
 def addDialect(lang):
-     if lang=="en":
-        return "en-CA"
-     if lang=="fr":
-        return "fr-CA"
-    if lang=="hi":
-        return "hi-IN"
-    if lang=="ja":
-        return "ja-JP"
-    if lang=="es":
-        return "es-ES"
-    if lang=="cmn":
-        return "cmn-CN"
+	if lang=="en":
+		return "en-CA"
+	if lang=="fr":
+		return "fr-CA"
+	if lang=="hi":
+		return "hi-IN"
+	if lang=="ja":
+		return "ja-JP"
+	if lang=="es":
+		return "es-ES"
+	if lang=="cmn":
+		return "cmn-CN"
 
 
 
