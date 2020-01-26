ec5743aac308bfb599c400e5008ec1a8c702ab41
Kyle Pinto <kylepinto@yahoo.ca>
2020-01-26 01:49:39 -0500

    Simplified speech2text

--- speech2text/s2t-test.py
+++ speech2text/s2t-test.py
@@ -1,22 +1,4 @@
-from speech2text import Speech2Text
-from ffmpeg_test import decode_audio
+import speech2text
 
-# create Speech2Text object
-s2t = Speech2Text()
+speech2text.transcribe("/dev/stdout")
 
-# obtain raw audio format
-raw_audio = decode_audio("input2.mp4")
-
-# load audio file
-s2t.load(raw_audio)
-
-# specify audio file details
-s2t.config(
-	encoding = Speech2Text.DEFAULT_ENCODING,
-	sample_rate = Speech2Text.DEFAULT_SAMPLE_RATE_HERTZ,
-	language = Speech2Text.DEFAULT_INPUT_LANGUAGE
-	)
-
-# translate and save transcript
-s2t.translate("output2")
-print()
--- speech2text/speech2text.py
+++ speech2text/speech2text.py
@@ -1,45 +1,55 @@
+# I AM the Senate
+
 import io
 import os
+import ffmpeg
+import sys
 
 # Imports the Google Cloud client library
 from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
 
-class Speech2Text:
+AUDIO_ENCODING = enums.RecognitionConfig.AudioEncoding.LINEAR16
+SAMPLE_RATE = 16000   # (in Hertz)
+INPUT_LANGUAGE = 'en-US'
+VIDEO_FILE = 'input2.mp4'
 
-	DEFAULT_ENCODING = enums.RecognitionConfig.AudioEncoding.LINEAR16
-	DEFAULT_SAMPLE_RATE_HERTZ = 16000
-	DEFAULT_INPUT_LANGUAGE = 'en-US'
-	DEFAULT_AUDIO_FILE = 'input.flac'
+audio_info = types.RecognitionConfig(
+		encoding = AUDIO_ENCODING,
+		sample_rate_hertz = SAMPLE_RATE,
+		language_code = INPUT_LANGUAGE)
+speech2text_client = speech.SpeechClient()
 
-	# CONSTRUCTOR
-	def __init__(self):
-		print("Initializing Speech-to-Text .......")
-		self.client = speech.SpeechClient() # Instantiates a client
-		self.audio = None
-		self.info = None
+# decodes an mp4 video into raw audio
+def extract_audio(in_filename, **input_kwargs):
+	try:
+		raw_audio, err = (ffmpeg
+			.input(in_filename, **input_kwargs)
+			.output('-', format = 's16le', acodec = 'pcm_s16le', ac = 1, ar = '16k')
+			.overwrite_output()
+			.run(capture_stdout = True, capture_stderr = True)
+		)
+	except ffmpeg.Error as e:
+		print(e.stderr, file = sys.stderr)
+		sys.exit(1)
+	return types.RecognitionAudio(content = raw_audio)
 
-	# Loads the audio into memory and returns a handle
-	def load(self, raw_audio):
-		print("Loading audio file .......")
-		self.audio = types.RecognitionAudio(content = raw_audio)
+# transcribes the audio to text and saves transcript to a file
+def transcribe(file_name):
+	print("Audio input settings:")
+	print("Audio Encoding:    %s" % AUDIO_ENCODING)
+	print("Sample Rate (Hz):  %s" % SAMPLE_RATE)
+	print("Input Language:    %s" % INPUT_LANGUAGE)
 
-	# sets the audio encoding, sample rate (Hz), and language
-	def config(self, encoding, sample_rate, language):
-		print("Audio input details:")
-		print("Encoding:          %s" % encoding)
-		print("Sample Rate (Hz):  %s" % sample_rate)
-		print("Language:          %s" % language)
-		self.info = types.RecognitionConfig(encoding = encoding, sample_rate_hertz = sample_rate, language_code = language)
+	print("Converting mp4 to raw audio .......")
+	audio = extract_audio(VIDEO_FILE)
 
-	# translates the audio and saves transcript to a file
-	def translate(self, file_name):
-		print("Translating .......")
-		file = open(file_name, "a")
-		response = self.client.recognize(self.info, self.audio)
-		for result in response.results:
-			file.write(result.alternatives[0].transcript)
+	print("Transcribing .......")
+	file = open(file_name, "a")
+	response = speech2text_client.recognize(audio_info, audio)
+	for result in response.results:
+		file.write(result.alternatives[0].transcript)
 
-		print("Saving transcript in '%s' ......." % file_name)
-		file.close()
+	print("Saving transcript in '%s' ......." % file_name)
+	file.close()
