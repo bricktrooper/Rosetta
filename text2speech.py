aba5e200b638902bddbe45b07b943870f819b133
Kyle Pinto <kylepinto@yahoo.ca>
2020-01-26 03:12:43 -0500

    Made s2t and t2s more modular

--- text2speech.py
+++ text2speech.py
@@ -4,36 +4,33 @@ Note: ssml must be well-formed according to:
         https://www.w3.org/TR/speech-synthesis/
 
 """
-def text_2_speech(translated_string,lang= 'en-US'):
 
-        from google.cloud import texttospeech
+from google.cloud import texttospeech
 
-        # Instantiates a client
-        client = texttospeech.TextToSpeechClient()
+OUTPUT_AUDIO_ENCODING = texttospeech.enums.AudioEncoding.MP3
+OUTPUT_LANGUAGE = 'en-US'
+OUTPUT_AUDIO_VOID_GENDER = texttospeech.enums.SsmlVoiceGender.NEUTRAL
+OUTPUT_AUDIO_FILE = 'output.mp3'
 
+# Instantiates a client
+text2speech_client = texttospeech.TextToSpeechClient()
 
+def speak(translated_string, lang = 'en-US'):
 
         # Set the text input to be synthesized
-        synthesis_input = texttospeech.types.SynthesisInput(text=translated_string)
+        synthesis_input = texttospeech.types.SynthesisInput(text = translated_string)
 
         # Build the voice request, select the language code ("en-US") and the ssml
-        # voice gender ("neutral")
-        voice = texttospeech.types.VoiceSelectionParams(
-                    language_code= lang,
-                        ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+        voice = texttospeech.types.VoiceSelectionParams(language_code = lang, ssml_gender = OUTPUT_AUDIO_VOID_GENDER)
 
         # Select the type of audio file you want returned
-        audio_config = texttospeech.types.AudioConfig(
-                    audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+        audio_config = texttospeech.types.AudioConfig(audio_encoding = OUTPUT_AUDIO_ENCODING)
 
-        # Perform the text-to-speech request on the text input with the selected
-        # voice parameters and audio file type
-        response = client.synthesize_speech(synthesis_input, voice, audio_config)
+        # Perform the text-to-speech request on the text input with the selected voice parameters and audio file type
+        response = text2speech_client.synthesize_speech(synthesis_input, voice, audio_config)
 
         # The response's audio_content is binary.
-        with open('audio_translated.mp3', 'wb') as out:
+        with open(OUTPUT_AUDIO_FILE, 'wb') as out:
                 # Write the response to the output file.
-                    out.write(response.audio_content)
-                    print('Audio content written to file "output.mp3"')
-
-                
+                out.write(response.audio_content)
+                print('Audio content written to file %s' % OUTPUT_AUDIO_FILE)
