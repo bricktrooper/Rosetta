591a20744f60e024533f83d6e286f744737737ed
Snehit <darkfire6601@gmail>
2020-01-26 09:30:55 -0500

    process updated to take in variables for mp4 file, input lang and target lang, 6 languages supported (en,fr,hi,es,cmn,ja)

--- process.py
+++ process.py
@@ -1,12 +1,41 @@
 # Have you ever heard the tragedy of Darth Plagueis the Wise?
 
-from speech2text import *
-from translate import *
-from text2speech import *
 
-transcript = transcribe("tests/trudeau.mp4", "tests/translated", "fr-CA")
+#return 3-tuple, 1st elem = name of translated mp3, 2nd elem = name of translated text, 3rd elem = original text
+# call with NO .p4 extension
+def process(mp4_file= "tests/trudeau", input_lang= "fr",target_lang="en"):
 
-str2 = translate(transcript, "en")
-print(str2)
 
-speak(str2, "en-CA", "tests/translated.mp3")
+    from speech2text import *
+    from translate import *
+    from text2speech import *
+
+    transcript = transcribe((mp4_file+".mp4"), "tests/translated", addDialect(input_lang))
+
+
+    translated_script = translate(transcript, target_lang)
+    print(translated_script)
+
+    mp3_translated_name =  mp4_file+"_translated.mp3"
+
+
+    speak(translated_script, addDialect(target_lang), mp3_translated_name)
+
+    return (mp3_translated_name, translated_scrjpt, transcript)
+
+def addDialect(lang):
+     if lang=="en":
+        return "en-CA"
+     if lang=="fr":
+        return "fr-CA"
+    if lang=="hi":
+        return "hi-IN"
+    if lang=="ja":
+        return "ja-JP"
+    if lang=="es":
+        return "es-ES"
+    if lang=="cmn":
+        return "cmn-CN"
+
+
+
